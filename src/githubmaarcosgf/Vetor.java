e86b872a50e73eb36bcadbbc80253e1430ec2819
IFSC <IFSC@CDR-770630.CEFETSC>
2018-03-28 22:05:16 -0300

    Versão 0.1.1 28/03/18

--- src/githubmaarcosgf/Vetor.java
+++ src/githubmaarcosgf/Vetor.java
@@ -5,46 +5,95 @@
  */
 package githubmaarcosgf;
 
+import java.util.Random;
+
 /**
  *
  * @author IFSC
  */
 public class Vetor {
-int vet[];
-            /* Construtor */
-            public Vetor(int size){
-                vet = new int[size];
+private int vet[];
+    private int contatenarVetor[];
+    private int size;
+    private int contador=0;
+    private boolean igualdade = false;
+    private int i=0;
+    private int tamanhoVetor;
+    /* Construtor */
+            public Vetor(int tamanho){
+               vet = new int[tamanho];
+               size = tamanho;
             }
             
             public void randomFill(){
-                Random r = new Random();
-                   for(int i=0; i<vet.lenght;i++){
-                       vet[i] = r.nextInt(1000);
-                   }
+                Random rand = new Random();
+                for(contador =0 ; contador < size ; contador ++){
+                     vet[contador] = 2 + rand.nextInt();
+        }
             }
             
-            public void bubbleSort(){}
+            public void bubbleSort(){
+                 int muda;
+                 int auxiliar;
+                 muda = 1;
+                    while (muda == 1){
+                           muda = 0;        
+                    for (int i = 0; i <= 3; i++){
+                        if (vet[i] > vet[i + 1]){
+                            muda = 1;
+                            auxiliar = vet[i];
+                            vet[i] = vet[i + 1];
+                            vet[i + 1] = auxiliar;
+               }
+           }
+        }
+      }
            
             public String toString(){
-                return null;   
+                String vetores[] = new String[size];
+                for(contador=0; contador<size;contador++){
+                    vetores[contador] = Integer.toString(vet[contador]) ;
+        }
+                    return vetores[size];  
             }
 
             public boolean equals(int[] v){
-                return false;
+                 for(contador=0; contador<size;contador++){
+                    if(vet[contador] == v[contador]){
+                        i++;
+                }
+            }
+                    if(i == size){
+                     igualdade = true;
+        }
+                return igualdade;
             }
       
-            public void setAt(int pos, int value){}
+            public void setAt(int posi, int value){
+              vet[posi] = value;
+            }
          
-            public int valueAt(int pos){
-                return 0;
+            public int valueAt(int posi){
+                return vet[posi];
             }
           
             public int length(){
-                return 0;
+                tamanhoVetor = vet.length;
+                return tamanhoVetor;
+    
             }
             
             public Vetor Concat(Vetor v){
-                return null; 
+                for(contador=0;contador<(size + v.length()); contador ++){
+                      if(contador<size){
+                      contatenarVetor[contador] = vet[contador];
+                }else{
+                      contatenarVetor[contador] = vet[contador];
+                     }
+                }
+                return null;
+                 
+                }     
             }
     
-}
+
